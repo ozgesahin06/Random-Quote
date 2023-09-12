ada97fbb88422cc831f6df2e7f3a10f9887706c1
ozgesahin06 <ozge.sahin0671@gmail.com>
2023-09-12 21:04:39 +0300

    random quote

---
+++ random-quote/src/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const [quotes, setQuotes] = useState ("");
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then((res)=>res.json())
+    .then(data=>{
+      let randomNum = Math.floor(Math.random()*data.length)
+      setQuotes(data[randomNum])
+    })
+  }
+  useEffect(()=>{
+    getQuote();
+  }, [])
+  return (
+    <div className="App">
+      <div className='quote'>
+        <p>{quotes.text}</p>
+        <p>{quotes.author}</p>
+      </div>
+      <div className='btnContainer'>
+        <button onClick={getQuote} className='btn'>Get Quote</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
